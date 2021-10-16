0c10ebc790d790885eb82c92a3a1f0f1c4ccaeeb
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-10-16 16:45:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+}
+function setup() {
+    canvas = createCanvas(600, 370);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R_3JGtLfr/model.json", modelLoaded);
+}
+function draw() {
+    image(video, 0, 0, 600, 370);
+    classifier.classify(video, getResults);
+}
+function modelLoaded() {
+    console.log("model loaded!");
+}
+function getResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
